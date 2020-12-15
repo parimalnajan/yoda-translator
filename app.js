28b9b1a912dff99241c146694542696c73ff304f
parimalnajan <63017672+parimalnajan@users.noreply.github.com>
2020-12-15 19:33:11 +0530

    used throw/catch
    Used throw to handle and display error

--- app.js
+++ app.js
@@ -13,23 +13,24 @@ function getUrl(text){
 }
 
 function errorHandler(error){
-console.log(error+" error occured");
+console.log(" error occured"+error);
+alert('A server error occured, please try later');
 outputDiv.innerText=error;
 }
 
 function clickHandler(){
-    console.log("click zala eree")
+    //console.log("click test")
     // console.log(txtInput+txtInput.value);
 
     fetch(getUrl(txtInput.value))
     .then(response=>response.json()//{})
     .then(json=>{
-
-        if(response.status===429){throw json.error.message}else{
-
+        if(response.status===429){
+            throw json.error.message;
+        }else{
         var translated=json.contents.translated;
-        outputDiv.innerText=translated;
         console.log("answer: "+translated);}
+        outputDiv.innerText=translated;
     })
     .catch(errorHandler)
     );
